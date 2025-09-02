ce69e65c0a2e7b20d55b1a2418a385ecebfd8ea1
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-02 21:14:49 +0200

    Add parser class with empty methods

---
+++ src/parsing/parser.ts
@@ -0,0 +1,13 @@
+import { textChunk } from "./textChunk";
+
+export class Parser {
+    chunkifyText(input: string): textChunk[] {
+        // TODO: Implement parsing logic.
+        throw new Error("Method not implemented.");
+    }
+
+    mergeChunks(inputChunks: textChunk[]): textChunk[] {
+        // TODO: Implement parsing logic.
+        throw new Error("Method not implemented.");
+    }
+}
