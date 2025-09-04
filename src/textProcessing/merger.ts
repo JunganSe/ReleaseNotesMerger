d85bb816caf176806c3e18fdd8fab46c543820f6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-04 22:45:33 +0200

    Merger initialization

--- src/textProcessing/merger.ts
+++ src/textProcessing/merger.ts
@@ -1,6 +1,20 @@
+import { MergerOptions } from "./mergerOptions";
 import { textChunk } from "./textChunk";
 
 export class Merger {
+    options: MergerOptions;
+
+    constructor() {
+        this.options = this.getDefaultOptions();
+    }
+
+    private getDefaultOptions(): MergerOptions {
+        return {
+            headingOrder: [],
+            date: undefined
+        }
+    }
+
     mergeChunks(inputChunks: textChunk[]): textChunk[] {
         // TODO: Implement merging logic.
         throw new Error("Method not implemented.");
