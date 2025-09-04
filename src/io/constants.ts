15458c448c2c2f029f19fd71100bb6694667c479
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-04 23:02:55 +0200

    Add html element ids to constants

---
+++ src/io/constants.ts
@@ -0,0 +1,6 @@
+export class HtmlElementId {
+    static readonly inputTextarea = "textarea-input";
+    static readonly outputTextarea = "textarea-output";
+    static readonly mergeButton = "btn-merge";
+    static readonly copyButton = "btn-copy";
+}
