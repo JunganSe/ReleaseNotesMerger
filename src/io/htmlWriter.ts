e14ec48330d7aad3e914d9f83524fb9df2a7c412
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-06 20:13:06 +0200

    Implement writeOutputText

--- src/io/htmlWriter.ts
+++ src/io/htmlWriter.ts
@@ -1,6 +1,9 @@
+import { HtmlElementId } from "./constants";
+
 export class HtmlWriter {
     writeOutputText(text: string): void {
-        // TODO: Implement writing to textarea.
-        throw new Error("Method not implemented.");
+        const textarea = document.getElementById(HtmlElementId.outputTextarea) as HTMLTextAreaElement;
+        if (textarea)
+            textarea.value = text;
     }
 }
