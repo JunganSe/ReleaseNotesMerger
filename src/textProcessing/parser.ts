cd4a153009100ae71f2f66d2b9cb4f5cad99a408
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-08 22:25:59 +0200

    Implement chunkifyText

--- src/textProcessing/parser.ts
+++ src/textProcessing/parser.ts
@@ -13,10 +13,15 @@ export class Parser {
     }
 
     chunkifyText(input: string): textChunk[] {
-        // TODO: Implement parsing logic.
-        // - Divide into chunks based on empty lines.
-        // - Find headings in each chunk. Should be the first line.
-        // - Store everything else as content, one string per line.
-        throw new Error("Method not implemented.");
+        const inputChunks = input.split(/\r?\n\r?\n/); // Splits on double line breaks. (Empty lines)
+        const chunks: textChunk[] = inputChunks.map(inputChunk => {
+            const lines = inputChunk
+                .split(/\r?\n/) // Split on line breaks.
+                .filter(line => !line.startsWith('#'));
+            const heading = (lines.length > 0) ? lines[0] : null; // Use first line as heading.
+            const content = (lines.length > 1) ? lines.slice(1) : []; // Use all lines except first as content.
+            return { heading, content }
+        });
+        return chunks.filter(chunk => chunk.content.length > 0);
     }
 }
