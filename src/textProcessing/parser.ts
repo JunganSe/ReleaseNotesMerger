a608f64def3ca2ebf27d6f5c3ee4c7fd0eb880d4
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-08 22:51:07 +0200

    Refactor parseTextChunk

--- src/textProcessing/parser.ts
+++ src/textProcessing/parser.ts
@@ -3,16 +3,23 @@ import { TextChunk } from "./textChunk";
 export class Parser {
     chunkifyText(input: string): TextChunk[] {
         const inputChunks = input.split(/\r?\n\r?\n/); // Split on double line breaks. (Empty lines)
-        const chunks: TextChunk[] = inputChunks.map(this.parseChunk);
+        const chunks: TextChunk[] = inputChunks.map(this.parseTextChunk);
         return chunks.filter(chunk => chunk.content.length > 0);
     }
 
-    private parseChunk(inputChunk: string): TextChunk {
-        const lines = inputChunk
+    private parseTextChunk(input: string): TextChunk {
+        if (!input?.trim())
+            return { heading: null, content: [] };
+
+        const lines = input
             .split(/\r?\n/) // Split on line breaks.
             .filter(line => !line.startsWith('#'));
-        const heading = (lines.length > 0) ? lines[0] : null; // Use the first line as heading.
-        const content = (lines.length > 1) ? lines.slice(1) : []; // Use all lines except the first as content.
-        return { heading, content }
+
+        if (lines.length === 0)
+            return { heading: null, content: [] };
+
+        const heading = lines[0];
+        const content = lines.slice(1);
+        return { heading, content };
     }
 }
