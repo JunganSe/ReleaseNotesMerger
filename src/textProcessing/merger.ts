6d2132f29e12191c2bb481090ee4e24a0eb28274
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-11 22:26:11 +0200

    Reorganize mergeChunks content

--- src/textProcessing/merger.ts
+++ src/textProcessing/merger.ts
@@ -9,24 +9,16 @@ export class Merger {
     }
 
     mergeChunks(inputChunks: TextChunk[]): TextChunk[] {
-        // TODO: Implement merging logic.
-        // TODO: Implement options.indentSize
-
-        // x Get all headings into an array
-        // - Create an outputChunk for each heading
-        // - Append inputContent to the correct outputChunk based on heading
-        // - Return outputChunks
         const uniqueHeadings = this.getUniqueHeadings(inputChunks);
+        const outputChunks: TextChunk[] = uniqueHeadings.map(heading => ({ heading, content: [] }));
+        inputChunks.forEach(inputChunk => this.addContentToMatchingChunk(inputChunk, outputChunks));
+
         // TODO: If options.ignoreHeadingCase is true, remove duplicates that differ only by case. (Prefer Capitalized versions.)
         // TODO: If options.allowMisspelledHeadings is true, group similar headings together.
         //       (e.g. "Feature", "Features", "Feautres" -> "Features")
         //       If options.headingOrder contains a similar heading, use that.
         // TODO: Order uniqueHeadings according to options.headingOrder
-        
-        const outputChunks: TextChunk[] = uniqueHeadings.map(heading => ({ heading, content: [] }));
-
-        inputChunks.forEach(inputChunk =>
-            this.addContentToMatchingChunk(inputChunk, outputChunks));
+        // TODO: Implement options.indentSize
 
         return outputChunks;
     }
