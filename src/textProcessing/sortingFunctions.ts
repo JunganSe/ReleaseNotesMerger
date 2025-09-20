90b89d3d1e6d8fd15f0d4ee96e5fef4ac1ec677a
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-20 21:53:46 +0200

    Whitespace

--- src/textProcessing/sortingFunctions.ts
+++ src/textProcessing/sortingFunctions.ts
@@ -2,16 +2,14 @@ export class SortingFunctions {
     static SortByHeadingOrder(a: string, b: string, headingOrder: string[]): number {
         const indexA = headingOrder.indexOf(a);
         const indexB = headingOrder.indexOf(b);
-        if (indexA === -1 && indexB === -1) {
-            return 0; // Both headings not found in the order list, maintain their relative order.
-        } else if (indexA === -1) {
+
+        if (indexA === -1 && indexB === -1)
+            return 0; // Neither heading found, maintain their relative order.
+        else if (indexA === -1)
             return 1; // a not found, b found, b comes first.
-        }
-        else if (indexB === -1) {
+        else if (indexB === -1)
             return -1; // b not found, a found, a comes first.
-        }
-        else {
+        else
             return indexA - indexB; // Both found, sort by their indices.
-        }
     }
 }
