f6c56ecf16d77bce3897d264a1b170f996b50d1c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-21 22:16:49 +0200

    Early version of chunk combining by casing

--- src/textProcessing/merger.ts
+++ src/textProcessing/merger.ts
@@ -18,6 +18,40 @@ export class Merger {
         const outputChunks: TextChunk[] = uniqueHeadings.map(heading => ({ heading, content: [] }));
         inputChunks.forEach(inputChunk => this.addContentToMatchingOutputChunk(inputChunk, outputChunks));
 
+        // Combine chunks with same heading but different casing, if ignoreHeadingCase is true.
+        // Prefer versions in headingOrder, then prefer capitalized versions, then prefer the first one.
+        outputChunks.forEach(chunk => {
+            if (!this._options.ignoreHeadingCase || !chunk.heading || !chunk.content.length)
+                return;
+
+            const matchingChunk = outputChunks.find(c => c !== chunk && c.heading?.toLowerCase() === chunk.heading?.toLowerCase());
+            if (!matchingChunk)
+                return;
+
+            const isChunkCapitalized = (chunk.heading && chunk.heading[0] === chunk.heading[0].toUpperCase());
+            const isMatchingChunkCapitalized = (matchingChunk.heading && matchingChunk.heading[0] === matchingChunk.heading[0].toUpperCase());
+            const isChunkInHeadingOrder = this._options.headingOrder.length > 0
+                && this._options.headingOrder.some(h => h.toLowerCase() === chunk.heading?.toLowerCase());
+            const isMatchingChunkInHeadingOrder = this._options.headingOrder.length > 0
+                && this._options.headingOrder.some(h => h.toLowerCase() === matchingChunk.heading?.toLowerCase());
+
+            const keeper = (isChunkInHeadingOrder)
+                ? chunk
+                : (isMatchingChunkInHeadingOrder)
+                    ? matchingChunk
+                    : (isChunkCapitalized)
+                        ? chunk
+                        : (isMatchingChunkCapitalized)
+                            ? matchingChunk
+                            : chunk;
+            const goner = (keeper === chunk)
+                ? matchingChunk
+                : chunk;
+
+            keeper.content.push(...goner.content);
+            goner.content = [];
+        });
+
         // TODO: If options.ignoreHeadingCase is true, remove duplicates that differ only by case and move the content to the one being kept. (Prefer Capitalized versions.)
         // TODO: If options.allowMisspelledHeadings is true, group similar headings together.
         //       (e.g. "Feature", "Features", "Feautres" -> "Features")
