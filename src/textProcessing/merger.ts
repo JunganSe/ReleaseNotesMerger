024eb6711b39da10b01b5db2fc68c636a8eb4a25
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-22 22:27:27 +0200

    Refactor chunk merging by heading

--- src/textProcessing/merger.ts
+++ src/textProcessing/merger.ts
@@ -18,13 +18,37 @@ export class Merger {
         const outputChunks: TextChunk[] = uniqueHeadings.map(heading => ({ heading, content: [] }));
         inputChunks.forEach(inputChunk => this.addContentToMatchingOutputChunk(inputChunk, outputChunks));
 
-        // Combine chunks with same heading but different casing, if ignoreHeadingCase is true.
-        // Prefer versions in headingOrder, then prefer capitalized versions, then prefer the first one.
-        // TODO: Improve and refactor.
-        //       Handle cases where there are more than two chunks with same heading but different casing.
-        //       Optimize performance.
-        outputChunks.forEach(chunk => {
-            if (!this._options.ignoreHeadingCase || !chunk.heading || !chunk.content.length)
+        if (this._options.ignoreHeadingCase)
+            this.mergeChunksWithSameHeading_CaseInsensitive(outputChunks);
+
+        // TODO: If options.allowMisspelledHeadings is true, group similar headings together.
+        //       (e.g. "Feature", "Features", "Feautres" -> "Features")
+        //       If options.headingOrder contains a similar heading, use that.
+
+        return outputChunks.filter(chunk => chunk.heading && chunk.content.length > 0);
+    }
+
+    private getUniqueHeadings(chunks: TextChunk[]): string[] {
+        const headings = chunks
+            .map(chunk => chunk.heading)
+            .filter(heading => heading != null);
+        return [...new Set(headings)];
+    }
+
+    private addContentToMatchingOutputChunk(inputChunk: TextChunk, outputChunks: TextChunk[]): void {
+        const outputChunk = outputChunks.find(chunk => chunk.heading === inputChunk.heading);
+        outputChunk?.content.push(...inputChunk.content);
+    }
+
+    // Combine chunks with same heading but different casing, if ignoreHeadingCase is true.
+    // Prefer versions in headingOrder, then prefer capitalized versions, then prefer the first one.
+    // TODO: Improve and refactor.
+    //       Handle cases where there are more than two chunks with same heading but different casing.
+    //       Optimize performance.
+    private mergeChunksWithSameHeading_CaseInsensitive(chunks: TextChunk[]): TextChunk[] {
+        const outputChunks: TextChunk[] = [];
+        chunks.forEach(chunk => {
+            if (!chunk.heading || !chunk.content.length)
                 return;
 
             const matchingChunk = outputChunks.find(c => c !== chunk && c.heading?.toLowerCase() === chunk.heading?.toLowerCase());
@@ -54,23 +78,6 @@ export class Merger {
             keeper.content.push(...goner.content);
             goner.content = [];
         });
-
-        // TODO: If options.allowMisspelledHeadings is true, group similar headings together.
-        //       (e.g. "Feature", "Features", "Feautres" -> "Features")
-        //       If options.headingOrder contains a similar heading, use that.
-
-        return outputChunks.filter(chunk => chunk.heading && chunk.content.length > 0);
-    }
-
-    private getUniqueHeadings(chunks: TextChunk[]): string[] {
-        const headings = chunks
-            .map(chunk => chunk.heading)
-            .filter(heading => heading != null);
-        return [...new Set(headings)];
-    }
-
-    private addContentToMatchingOutputChunk(inputChunk: TextChunk, outputChunks: TextChunk[]): void {
-        const outputChunk = outputChunks.find(chunk => chunk.heading === inputChunk.heading);
-        outputChunk?.content.push(...inputChunk.content);
+        return outputChunks;
     }
 }
