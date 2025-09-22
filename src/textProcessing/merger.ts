c10614b687faaf818275f892ee36ae7826bda202
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-22 22:40:50 +0200

    Partial refactor of chunk merging by heading

--- src/textProcessing/merger.ts
+++ src/textProcessing/merger.ts
@@ -19,7 +19,7 @@ export class Merger {
         inputChunks.forEach(inputChunk => this.addContentToMatchingOutputChunk(inputChunk, outputChunks));
 
         if (this._options.ignoreHeadingCase)
-            this.mergeChunksWithSameHeading_CaseInsensitive(outputChunks);
+            this.mergeAllChunksWithSameHeading_CaseInsensitive(outputChunks);
 
         // TODO: If options.allowMisspelledHeadings is true, group similar headings together.
         //       (e.g. "Feature", "Features", "Feautres" -> "Features")
@@ -40,47 +40,48 @@ export class Merger {
         outputChunk?.content.push(...inputChunk.content);
     }
 
+    // Mutates the provided chunks array.
+    private mergeAllChunksWithSameHeading_CaseInsensitive(chunks: TextChunk[]): void {
+        chunks.forEach(chunk => this.mergeChunksWithSameHeading_CaseInsensitive(chunk, chunks));
+    }
+
     // Combine chunks with same heading but different casing, if ignoreHeadingCase is true.
     // Prefer versions in headingOrder, then prefer capitalized versions, then prefer the first one.
     // TODO: Improve and refactor.
     //       Handle cases where there are more than two chunks with same heading but different casing.
     //       Optimize performance.
-    private mergeChunksWithSameHeading_CaseInsensitive(chunks: TextChunk[]): TextChunk[] {
-        const outputChunks: TextChunk[] = [];
-        chunks.forEach(chunk => {
-            if (!chunk.heading || !chunk.content.length)
-                return;
+    private mergeChunksWithSameHeading_CaseInsensitive(chunk: TextChunk, chunks: TextChunk[]): void {
+        if (!chunk.heading || !chunk.content.length)
+            return;
 
-            const matchingChunks = this.getOtherChunksWithSameHeading_CaseInsensitive(chunk, chunks);
-            if (!matchingChunks.length)
-                return;
+        const matchingChunks = this.getOtherChunksWithSameHeading_CaseInsensitive(chunk, chunks);
+        if (!matchingChunks.length)
+            return;
 
-            // const matchingChunk = outputChunks.find(c => c !== chunk && c.heading?.toLowerCase() === chunk.heading?.toLowerCase());
-            
-            // const isChunkCapitalized = (chunk.heading && chunk.heading[0] === chunk.heading[0].toUpperCase());
-            // const isMatchingChunkCapitalized = (matchingChunk.heading && matchingChunk.heading[0] === matchingChunk.heading[0].toUpperCase());
-            // const isChunkInHeadingOrder = this._options.headingOrder.length > 0
-            //     && this._options.headingOrder.some(h => h.toLowerCase() === chunk.heading?.toLowerCase());
-            // const isMatchingChunkInHeadingOrder = this._options.headingOrder.length > 0
-            //     && this._options.headingOrder.some(h => h.toLowerCase() === matchingChunk.heading?.toLowerCase());
+        // const matchingChunk = outputChunks.find(c => c !== chunk && c.heading?.toLowerCase() === chunk.heading?.toLowerCase());
 
-            // const keeper = (isChunkInHeadingOrder)
-            //     ? chunk
-            //     : (isMatchingChunkInHeadingOrder)
-            //         ? matchingChunk
-            //         : (isChunkCapitalized)
-            //             ? chunk
-            //             : (isMatchingChunkCapitalized)
-            //                 ? matchingChunk
-            //                 : chunk;
-            // const goner = (keeper === chunk)
-            //     ? matchingChunk
-            //     : chunk;
+        // const isChunkCapitalized = (chunk.heading && chunk.heading[0] === chunk.heading[0].toUpperCase());
+        // const isMatchingChunkCapitalized = (matchingChunk.heading && matchingChunk.heading[0] === matchingChunk.heading[0].toUpperCase());
+        // const isChunkInHeadingOrder = this._options.headingOrder.length > 0
+        //     && this._options.headingOrder.some(h => h.toLowerCase() === chunk.heading?.toLowerCase());
+        // const isMatchingChunkInHeadingOrder = this._options.headingOrder.length > 0
+        //     && this._options.headingOrder.some(h => h.toLowerCase() === matchingChunk.heading?.toLowerCase());
 
-            // keeper.content.push(...goner.content);
-            // goner.content = [];
-        });
-        return outputChunks;
+        // const keeper = (isChunkInHeadingOrder)
+        //     ? chunk
+        //     : (isMatchingChunkInHeadingOrder)
+        //         ? matchingChunk
+        //         : (isChunkCapitalized)
+        //             ? chunk
+        //             : (isMatchingChunkCapitalized)
+        //                 ? matchingChunk
+        //                 : chunk;
+        // const goner = (keeper === chunk)
+        //     ? matchingChunk
+        //     : chunk;
+
+        // keeper.content.push(...goner.content);
+        // goner.content = [];
     }
 
     private getOtherChunksWithSameHeading_CaseInsensitive(chunk: TextChunk, chunks: TextChunk[]): TextChunk[] {
