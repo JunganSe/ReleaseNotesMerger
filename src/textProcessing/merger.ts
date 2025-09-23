3da11564e7b0bb0baf91fb67c70684321b97f790
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-23 18:33:42 +0200

    Refactor chunk merging by heading

--- src/textProcessing/merger.ts
+++ src/textProcessing/merger.ts
@@ -54,13 +54,12 @@ export class Merger {
         if (!chunk.heading || !chunk.content.length)
             return;
 
-        const matchingChunks = this.getChunksWithHeading_CaseInsensitive(chunks, chunk.heading);
+        const matchingChunks = this.getChunksWithMatchingHeading_CaseInsensitive(chunks, chunk.heading);
         if (!matchingChunks.length)
             return;
 
         const preferredChunk = this.getFirstChunkWithMatchingHeading_CaseInsensitive(matchingChunks, this._options.headingOrder);
-        const firstChunkWithCapitalizedHeading = matchingChunks.find(c =>
-            c.heading && c.heading[0] === c.heading[0].toUpperCase());
+        const firstChunkWithCapitalizedHeading = this.getFirstChunkWithCapitalizedHeading(matchingChunks);
 
         let keeper = preferredChunk ?? firstChunkWithCapitalizedHeading ?? chunk;
         const goners = matchingChunks.filter(c => c !== keeper);
@@ -73,11 +72,15 @@ export class Merger {
         });
     }
 
-    private getChunksWithHeading_CaseInsensitive(chunks: TextChunk[], heading: string): TextChunk[] {
+    private getChunksWithMatchingHeading_CaseInsensitive(chunks: TextChunk[], heading: string): TextChunk[] {
         return chunks.filter(c => c.heading?.toLowerCase() === heading.toLowerCase());
     }
 
     private getFirstChunkWithMatchingHeading_CaseInsensitive(chunks: TextChunk[], headings: string[]): TextChunk | undefined {
         return chunks.find(c => headings.some(h => h.toLowerCase() === c.heading?.toLowerCase()));
+    }
+
+    private getFirstChunkWithCapitalizedHeading(chunks: TextChunk[]): TextChunk | undefined {
+        return chunks.find(c => c.heading && c.heading[0] === c.heading[0].toUpperCase());
     }
 }
