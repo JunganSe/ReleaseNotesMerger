f5d618e3cff07329ce6176fb42f0d1855a82d358
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-09-24 21:49:17 +0200

    Remove empty chunks when merging

--- src/textProcessing/headingBasedChunkMerger.ts
+++ src/textProcessing/headingBasedChunkMerger.ts
@@ -9,6 +9,7 @@ export class HeadingBasedChunkMerger {
     */
     static mergeAllChunksWithSameHeading_CaseInsensitive(chunks: TextChunk[], preferredHeadings: string[]): void {
         chunks.forEach(chunk => this.mergeChunksWithSameHeading_CaseInsensitive(chunk, chunks, preferredHeadings));
+        this.removeEmptyChunks(chunks);
     }
 
     private static mergeChunksWithSameHeading_CaseInsensitive(chunk: TextChunk, chunks: TextChunk[], preferredHeadings: string[]): void {
@@ -47,5 +48,12 @@ export class HeadingBasedChunkMerger {
             goner.heading = null;
             goner.content = [];
         });
+    }
+
+    private static removeEmptyChunks(chunks: TextChunk[]): void {
+        for (let i = chunks.length - 1; i >= 0; i--) {
+            if (!chunks[i].heading || chunks[i].content.length === 0)
+                chunks.splice(i, 1);
+        }
     }
 }
