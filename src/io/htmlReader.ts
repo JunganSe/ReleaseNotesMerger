5e630733f2f18851a1d66152329047ec2b963058
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-10-13 21:44:39 +0200

    Read indent multiplier option

--- src/io/htmlReader.ts
+++ src/io/htmlReader.ts
@@ -27,6 +27,7 @@ export class HtmlReader {
     static getMergerOptions(): MergerOptions {
         return {
             headingOrder: this.getHeadingOrder(),
+            indentMultiplier: this.getIndentMultiplier(),
             ignoreHeadingCase: this.getIgnoreHeadingCase(),
             allowMisspelledHeadings: this.getAllowMisspelledHeadings(),
         };
@@ -65,6 +66,10 @@ export class HtmlReader {
             .map(str => str.trim())
             .filter(str => str.length)
             ?? [];
+    }
+
+    static getIndentMultiplier(): number {
+        return this.readInput_Number(HtmlElementId.IndentMultiplier) ?? 1;
     }
 
     static getIgnoreHeadingCase(): boolean {
