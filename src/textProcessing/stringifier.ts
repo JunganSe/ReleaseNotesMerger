1aba996d303383577db13a4d21dbb2314129227c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-10-22 21:53:57 +0200

    Only output date line if date has content

--- src/textProcessing/stringifier.ts
+++ src/textProcessing/stringifier.ts
@@ -17,7 +17,10 @@ export class Stringifier {
     private getDateString(): string | null {
         const prefix = this._options.datePrefix;
         const date = this._options.date?.toISOString().split('T')[0];
-        return [prefix, date].join(' ').trim() || null;
+
+        return (date)
+            ? [prefix ?? '', date].join(' ').trim()
+            : null;
     }
 
     private stringifyChunks(chunks: TextChunk[]): string {
